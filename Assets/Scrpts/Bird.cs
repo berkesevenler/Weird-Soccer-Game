86ff372e060c712fe77168ac61f1d74728e0ebfa
Berke Sevenler <berkeseveenlr@gmail.com>
2024-11-15 23:09:10 +0100

    Add files via upload

---
+++ Assets/Scrpts/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour 
+{
+	public float upForce = 200f;
+
+	private bool isDead = false;
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+	void Start()
+	{
+
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update()
+	{
+
+		if (isDead == false)
+		{
+
+			if (Input.GetMouseButtonDown(0))
+			{
+
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce (new Vector2(0, upForce));
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+
+		rb2d.velocity = Vector2.zero;
+		isDead = true;
+		GameControl.instance.BirdDied ();
+
+		}
+	}
